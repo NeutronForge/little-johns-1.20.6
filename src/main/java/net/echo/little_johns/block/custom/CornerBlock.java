126a5df0035335e914f430a7ba53811b090da2f4
Echo <71904242+NeutronForge@users.noreply.github.com>
2024-06-06 17:59:18 +0200

    changed: - small fixes

--- src/main/java/net/echo/little_johns/block/custom/CornerBlock.java
+++ src/main/java/net/echo/little_johns/block/custom/CornerBlock.java
@@ -1,15 +1,6 @@
 package net.echo.little_johns.block.custom;
 
 
-import com.google.common.collect.Maps;
-import net.minecraft.block.*;
-import net.minecraft.state.property.BooleanProperty;
-import net.minecraft.util.Util;
-import net.minecraft.util.math.Direction;
-import net.minecraft.util.shape.VoxelShape;
-
-import java.util.Map;
-
 
 public class CornerBlock {
 }
--- src/main/java/net/echo/little_johns/state/property/ModProperties.java
+++ src/main/java/net/echo/little_johns/state/property/ModProperties.java
@@ -1,10 +1,7 @@
 package net.echo.little_johns.state.property;
 
-import net.echo.little_johns.block.enums.CornerType;
-import net.minecraft.block.enums.SlabType;
-import net.minecraft.state.property.EnumProperty;
 import net.minecraft.state.property.Properties;
 
 public class ModProperties extends Properties {
-    public static final EnumProperty<CornerType> CORNER_TYPE = EnumProperty.of("type", CornerType.class);
+
 }
